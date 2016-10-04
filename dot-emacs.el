2fa4068ba6c01b414ac21c27e3191d3c8fa5b1ec
Tanner Hobson <thobson125@gmail.com>
2016-10-04 17:35:47 -0400

    Add marmalade package archive

--- dot-emacs.el
+++ dot-emacs.el
@@ -7,6 +7,7 @@
   (require 'package)
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
   (package-initialize)
 
   (let* ((packages
